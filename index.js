99348c52a613982d7b63a60ea271cbd198341b80
ArturrrL <artik.levitskyi18@gmail.com>
2024-11-11 12:18:43 +0200

    task completed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { program } = require('commander')
 const http = require('http')
+const path = require('path');
+const fs = require('fs').promises;
+const superagent = require('superagent');
+
 
 program
     .option('-h, --host <char>', 'server address')
@@ -8,8 +12,6 @@ program
 
 program.parse()
 const options = program.opts();
-
-
 if (!options.host) {
     console.error('Please enter server address');
     process.exit(1);
@@ -22,11 +24,86 @@ if (!options.cache) {
     console.error('Please enter path to cache files');
     process.exit(1);
 }
-const server = http.createServer((req, res) => {
-    res.writeHead(200)
-    res.end('Server is running!')
-})
 
+async function fetchImageFromHttpCat(statusCode)
+ {
+  try {
+    const response = await superagent.get(`https://http.cat/${statusCode}`);
+    return response.body;
+  } 
+  catch (err) 
+  {
+    throw new Error('Не вдалося отримати картинку з http.cat');
+  }
+}
+
+
+const server = http.createServer(async (req, res) =>
+   {
+  const statusCode = req.url.slice(1);
+  const filePath = path.join(options.cache, `${statusCode}.jpg`);
+
+
+if (req.method === 'GET') 
+    {
+    try 
+    {
+      const fileData = await fs.readFile(filePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(fileData);
+    } 
+    catch (err)
+    {
+      try 
+      {
+        const imageData = await fetchImageFromHttpCat(statusCode);
+        await fs.writeFile(filePath, imageData);
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        res.end(imageData);
+      } 
+      catch (error)
+       {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+      }
+    }
+  } 
+
+  else if (req.method === 'PUT')
+     {
+    let body = [];
+    req.on('data', chunk => body.push(chunk));
+    req.on('end', async () => {
+      const fileData = Buffer.concat(body);
+      try {
+        await fs.writeFile(filePath, fileData);
+        res.writeHead(201, { 'Content-Type': 'text/plain' });
+        res.end('Created');
+      } catch (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal server error');
+      }
+    }
+  );} 
+  
+  else if (req.method === 'DELETE') {
+    try {
+      await fs.unlink(filePath);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('OK');
+    } catch (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+    }
+  } 
+ 
+  else 
+  {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Method not allowed');
+  };
+
+   });
 server.listen(options.port, options.host, () => {
     console.log(`Server working on http://${options.host}:${options.port}`)
-})
+});
